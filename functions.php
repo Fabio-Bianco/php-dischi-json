236b75b8424e533c756bb5bea0ca3a19c469ac6c
Fabio-Bianco <biancobfabio@gmail.com>
2025-09-06 12:39:12 +0200

    Aggiungi file JSON e funzioni per gestire i dati dei dischi

---
+++ functions.php
@@ -0,0 +1,15 @@
+//funzione che restituisce il contenuto del file JSON
+
+<?php
+
+function getDisch() {
+
+    $dicscs_json = file_get_contents(__DIR__ . '/dischi.json');
+
+    return json_decode($dicscs_json, true); 
+
+}
+
+
+
+ ?>
